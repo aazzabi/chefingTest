b08cccf7c7809d82bcc6424556f6998e0c8f14fe
Arafet <arafet.azzabi@esprit.tn>
2020-08-21 23:38:38 +0100

    Cleaning allTasks page

--- client/src/app/Task/all/all.tasks.ts
+++ client/src/app/Task/all/all.tasks.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {EditTaskComponent} from "../edit/edit.task";
 import {TaskService} from "../../services/managers/task.service";
 import {AddTaskComponent} from "../add/add.task";
+import {Task} from '../../models/Task';
 
 @Component({
   selector: 'app-all-task-back',
@@ -12,16 +13,8 @@ import {AddTaskComponent} from "../add/add.task";
 })
 export class AllTasks {
   allTasks: Task[];
+  tableChecked = new Array;
 
-  collection = {
-    count: 10,
-    data: [],
-    dataEnCours: [],
-    dataEnAttente: [],
-    dataFermee: [],
-    dataConfirmed: [],
-    dataResolu: []
-  };
 
   constructor(
     private TaskService: TaskService,
@@ -31,18 +24,26 @@ export class AllTasks {
     this.allTasks = this.route.snapshot.data['tasks'];
   }
 
-  // tslint:disable-next-line:use-life-cycle-interface use-lifecycle-interface
-  ngOnInit() {
-    // @ts-ignore
-  }
-
-  edit(c: Task) {
+  edit(t: Task) {
     const modalRef = this.modalService.open(EditTaskComponent);
-    modalRef.componentInstance.c = c;
+    modalRef.componentInstance.t = t;
   }
 
   new() {
     const modalRef = this.modalService.open(AddTaskComponent);
+    modalRef.componentInstance.addTaskEvent.subscribe(($e) => {
+      console.log($e, "$e");
+      this.TaskService.addTask($e).subscribe(
+        response => {
+          console.log(JSON.parse(response));
+          modalRef.close();
+          this.allTasks.push(JSON.parse(response));
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    })
   }
 
   deleteTask(index: number, c: Task) {
@@ -58,109 +59,8 @@ export class AllTasks {
     }
   }
 
-  // checkbox(item: Task, n: number) {
-  //   if (this.tableChecked.find(x => x === item)) {
-  //     this.tableChecked.splice(this.tableChecked.indexOf(item), 1);
-  //     console.log(this.tableChecked);
-  //   } else {
-  //     this.tableChecked.push(item);
-  //     console.log(this.tableChecked);
-  //   }
-  // }
-
-  // deleteSelectedTasks() {
-  //   if (confirm('Are you sure to delete those Tasks !!! ')) {
-  //     const tempChecked = [];
-  //     this.tableChecked.forEach(e => tempChecked.push(e));
-  //     for (let i = 0; i < tempChecked.length; i++) {
-  //       const tcv = tempChecked[i];
-  //       const index = this.collection.data.findIndex(item => item.id === tcv.id);
-  //       this.TaskService.deleteTask(tcv).subscribe(
-  //           response => {
-  //           },
-  //           error => {
-  //             console.log(error);
-  //           }
-  //         );
-  //       this.collection.data.splice(index, 1);
-  //       const indexChecked = this.tableChecked.findIndex(item => item.id === tcv.id);
-  //       this.tableChecked.splice(indexChecked, 1);
-  //     }
-  //     tempChecked.forEach(obj => {
-  //       obj.checked = false;
-  //     });
-  //   }
-  // }
-
-  // archiverSelectedTasks() {
-  //   if (confirm('Are you sure to archive those Tasks !!! ')) {
-  //     const tempChecked = [];
-  //     this.tableChecked.forEach(e => tempChecked.push(e));
-  //     for (let i = 0; i < tempChecked.length; i++) {
-  //       const tcv = tempChecked[i];
-  //       const index = this.collection.data.findIndex(item => item.id === tcv.id);
-  //       this.TaskService.archiverTask(tcv).subscribe(
-  //           response => {
-  //           },
-  //           error => {
-  //             console.log(error);
-  //           }
-  //         );
-  //       this.collection.dataEnCours.splice(index, 1);
-  //       this.collection.dataEnAttente.splice(index, 1);
-  //       this.collection.dataResolu.splice(index, 1);
-  //       const indexChecked = this.tableChecked.findIndex(item => item.id === tcv.id);
-  //       this.tableChecked.splice(indexChecked, 1);
-  //     }
-  //     tempChecked.forEach(obj => {
-  //       obj.checked = false;
-  //       obj.status = 'FERME_SANS_SOLUTION';
-  //     });
-  //   }
-  // }
-  // desarchiverSelectedTasks() {
-  //   if (confirm('Are you sure to archive those Tasks !!! ')) {
-  //     const tempChecked = [];
-  //     // selectionner que les réclamation FERME_SANS_SOLUTION
-  //     this.tableChecked.forEach(e => {
-  //       if (e.status === 'FERME_SANS_SOLUTION') { tempChecked.push(e); }
-  //     });
-  //     for (let i = 0; i < tempChecked.length; i++) {
-  //       const tcv = tempChecked[i];
-  //       const index = this.collection.dataFermee.findIndex(item => item.id === tcv.id);
-  //       this.TaskService.desarchiverTask(tcv).subscribe(
-  //           response => {
-  //           },
-  //           error => {
-  //             console.log(error);
-  //           }
-  //         );
-  //       this.collection.dataFermee.splice(index, 1);
-  //       const indexChecked = this.tableChecked.findIndex(item => item.id === tcv.id);
-  //       this.tableChecked.splice(indexChecked, 1);
-  //     }
-  //     tempChecked.forEach(obj => {
-  //       obj.checked = false;
-  //       obj.status = 'EN_COURS';
-  //     });
-  //   }
-  // }
-
-  // bipper(c: any) {
-  //   this.TaskService.bipperAgent(c.responsable.id);
-  // }
-  newTask: any;
-
-  public addToList() {
-    if (this.newTask == '') {
-    } else {
-      this.allTasks.push(this.newTask);
-      this.newTask = '';
-    }
-  }
-
   check(t: Task) {
-    this.TaskService.confirmerTask(t).subscribe(
+    this.TaskService.confirmTask(t).subscribe(
       response => {
         // @ts-ignore
         t.status = 'DONE';
@@ -173,7 +73,7 @@ export class AllTasks {
   }
 
   unCheck(t: Task) {
-    this.TaskService.unConfirmerTask(t).subscribe(
+    this.TaskService.unConfirmTask(t).subscribe(
       response => {
         // @ts-ignore
         t.status = 'TODO';
@@ -183,5 +83,82 @@ export class AllTasks {
         console.log(error);
       }
     );
+  }
+
+  checkbox(item: any, i: any) {
+    if (this.tableChecked.find(x => x === item)) {
+      this.tableChecked.splice(this.tableChecked.indexOf(item), 1);
+    } else {
+      this.tableChecked.push(item);
+    }
+  }
+
+  markAsTODOSelectedClaims() {
+      const tempChecked = [];
+      this.tableChecked.forEach(e => tempChecked.push(e));
+      for (let i = 0; i < tempChecked.length; i++) {
+        const tcv = tempChecked[i];
+        const index = this.allTasks.findIndex(item => item._id === tcv._id);
+        this.TaskService.unConfirmTask(tcv).subscribe(
+          response => {
+            tcv.status = 'TODO';
+          },
+          error => {
+            console.log(error);
+          }
+        );
+        const indexChecked = this.tableChecked.findIndex(item => item._id === tcv._id);
+        this.tableChecked.splice(indexChecked, 1);
+      }
+      tempChecked.forEach(obj => {
+        obj.checked = false;
+      });
+  }
+
+  markAsDONESelectedClaims() {
+      const tempChecked = [];
+      this.tableChecked.forEach(e => tempChecked.push(e));
+      for (let i = 0; i < tempChecked.length; i++) {
+        const tcv = tempChecked[i];
+        const index = this.allTasks.findIndex(item => item._id === tcv._id);
+        this.TaskService.confirmTask(tcv).subscribe(
+          response => {
+            tcv.status = 'DONE';
+          },
+          error => {
+            console.log(error);
+          }
+        );
+        const indexChecked = this.tableChecked.findIndex(item => item._id === tcv._id);
+        this.tableChecked.splice(indexChecked, 1);
+      }
+      tempChecked.forEach(obj => {
+        obj.checked = false;
+      });
+  }
+
+  deleteSelectedClaims() {
+    if (confirm('Are you sure to delete those tasks !!! ')) {
+      const tempChecked = [];
+      this.tableChecked.forEach(e => tempChecked.push(e));
+      for (let i = 0; i < tempChecked.length; i++) {
+        const tcv = tempChecked[i];
+        const index = this.allTasks.findIndex(item => item._id === tcv._id);
+        console.log(index, 'index');
+        this.TaskService.deleteTask(tcv).subscribe(
+          response => {
+          },
+          error => {
+            console.log(error);
+          }
+        );
+        this.allTasks.splice(index, 1);
+        const indexChecked = this.tableChecked.findIndex(item => item._id === tcv._id);
+        this.tableChecked.splice(indexChecked, 1);
+      }
+      tempChecked.forEach(obj => {
+        obj.checked = false;
+      });
+    }
   }
 }
